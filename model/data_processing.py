dc0e85811fa72a625720b75ec7db0636ff7cdbaa
Jasmine Lebel <jasmine.lebel@live.com>
2023-03-15 09:59:31 -0400

    Import Split Data Files

--- model/data_processing.py
+++ model/data_processing.py
@@ -2,7 +2,7 @@
 from pyspark.rdd import RDD
 from pyspark.sql import DataFrame
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import desc, split
+from pyspark.sql.functions import desc
 
 import findspark
 
@@ -12,7 +12,7 @@ findspark.init()
 # Initialize a spark session.
 def init_spark():
     spark = (
-        SparkSession.builder.appName("Python Spark SQL basic example")
+        SparkSession.builder.appName("SOEN 471 Project")
         .config("spark.some.config.option", "some-value")
         .getOrCreate()
     )
@@ -41,84 +41,32 @@ def toCSVLine(data):
     return None
 
 
-def data_preparation(filename):
+def data_preparation(file1, file2, file3, file4, file5, file6):
     spark = init_spark()
 
-    df = spark.read.csv(filename, header=True)
+    df1 = spark.read.csv(file1, header=True)
+    df2 = spark.read.csv(file2, header=True)
+    df3 = spark.read.csv(file3, header=True)
+    df4 = spark.read.csv(file4, header=True)
+    df5 = spark.read.csv(file5, header=True)
+    df6 = spark.read.csv(file6, header=True)
+
+    df = df1.union(df2).union(df3).union(df4).union(df5).union(df6)
 
     # Start by selecting all field-related features relevant to our model and removing unnecessary characteristics
     # such as player name, height, age, net worth, etc.
-    players = df.select(
-        "long_name",
-        "player_positions",
-        "overall",
-        "skill_moves",
-        "pace",
-        "shooting",
-        "passing",
-        "dribbling",
-        "defending",
-        "physic",
-        "attacking_crossing",
-        "attacking_finishing",
-        "attacking_heading_accuracy",
-        "attacking_short_passing",
-        "attacking_volleys",
-        "skill_dribbling",
-        "skill_curve",
-        "skill_fk_accuracy",
-        "skill_long_passing",
-        "skill_ball_control",
-        "movement_acceleration",
-        "movement_sprint_speed",
-        "movement_agility",
-        "movement_reactions",
-        "movement_balance",
-        "power_shot_power",
-        "power_jumping",
-        "power_stamina",
-        "power_strength",
-        "power_long_shots",
-        "mentality_aggression",
-        "mentality_interceptions",
-        "mentality_positioning",
-        "mentality_vision",
-        "mentality_penalties",
-        "mentality_composure",
-        "defending_marking_awareness",
-        "defending_standing_tackle",
-        "defending_sliding_tackle",
-        "goalkeeping_diving",
-        "goalkeeping_handling",
-        "goalkeeping_kicking",
-        "goalkeeping_positioning",
-        "goalkeeping_reflexes",
-        "goalkeeping_speed",
+    players = df.count()
+
+    return players
+
+
+print(
+    data_preparation(
+        "./data/male_players1.csv",
+        "./data/male_players2.csv",
+        "./data/male_players3.csv",
+        "./data/male_players4.csv",
+        "./data/male_players5.csv",
+        "./data/male_players6.csv",
     )
-
-    # Most players have more than one preferred position, displayed in increasing order.
-    # We therefore filter through the positions and return only the most preferred,
-    # creating a new column and dropping the previous one.
-    players = players.withColumn(
-        "player_position", split(players["player_positions"], ",")[0]
-    ).drop("player_positions")
-
-    # For players whose position is not goalkeeper, most of these fields are empty.
-    # We will therefore remove all goal-keeping related attributes and goal-keepers
-    # from our data selection.
-    players = (
-        players.drop(
-            "goalkeeping_diving",
-            "goalkeeping_handling",
-            "goalkeeping_kicking",
-            "goalkeeping_positioning",
-            "goalkeeping_reflexes",
-            "goalkeeping_speed",
-        )
-        .filter(players.player_position != "GK")
-    )
-
-    return players.take(2)
-
-
-print(data_preparation("./data/male_players.csv"))
+)
